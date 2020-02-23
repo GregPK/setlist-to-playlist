ea4b92f7b42f6bc0f0efcc49e86a4d6b1600022b
GregPK <grzegorz.kaczorek@gmail.com>
2020-02-23 18:00:23 +0100

    Setup basic structure, fix formatting

--- src/setlist-to-playlist.cr
+++ src/setlist-to-playlist.cr
@@ -2,7 +2,7 @@ require "pathname"
 
 class S2pRb::Track
   property artist : String
-  property track_name : String 
+  property track_name : String
   property file_path : String
 
   def initialize(line, default_artist)
@@ -18,7 +18,7 @@ class S2pRb::Track
     filepath.dirname.to_s.match(/#{artist}/i) && filepath.basename.to_s.match(/#{track_name}/i)
   end
 
-  def parse_line(line,default_artist)
+  def parse_line(line, default_artist)
     tokens = line.split('-')
     if tokens.size == 2
       self.artist = tokens[0].strip
@@ -30,51 +30,50 @@ class S2pRb::Track
   end
 end
 
-
 class S2pRb::Setlist
-    property tracks : Array(Track)
-    property default_artist : String
+  property tracks : Array(Track)
+  property default_artist : String
 
-    def initialize(setlist_text)
-        @tracks = [] of Track
-        @default_artist = ""
+  def initialize(setlist_text)
+    @tracks = [] of Track
+    @default_artist = ""
     setlist_text.each_line do |line|
-        add_track(line)
+      add_track(line)
     end
     raise "No tracks found in setlist. [#{setlist_text}]" if tracks.nil? || tracks.empty?
-    end
+  end
 
-    def generate_playlist(base_path)
+  def generate_playlist(base_path)
     Dir.open(base_path) do
-        files = Dir.glob("**/*.{mp3,flac}")
-        tracks.each do |track|
+      files = Dir.glob("**/*.{mp3,flac}")
+      tracks.each do |track|
         candidates = [] of String
         files.each do |file|
-            if track.matches_file?(file)
+          if track.matches_file?(file)
             candidates << file
-            end
+          end
         end
         chosen_file = determine_best_file(candidates)
         track.file_path = chosen_file
-        end
+      end
     end
     tracks
-    end
+  end
 
-    def determine_best_file(candidates)
+  def determine_best_file(candidates)
     return candidates.first if candidates.size < 2
     candidates.sort_by do |candidate_file|
-        candidate_file.size
+      candidate_file.size
     end.first
-    end
+  end
 
-    def add_track(track_line)
+  def add_track(track_line)
     track = Track.new(track_line, default_artist)
     self.tracks << track
-    end
+  end
 end
 
 setlist = S2pRb::Setlist.new(STDIN.gets_to_end)
 playlist = setlist.generate_playlist(Dir.current)
-STDOUT << playlist.map{|t| t.file_path}.join("\n")
+STDOUT << playlist.map { |t| t.file_path }.join("\n")
 STDOUT << "\n"
